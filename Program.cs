b880424018f4653bf20d3e2b6bed96ac0b795318
thrashem <thrash@thrashem.com>
2025-04-24 19:31:01 +0900

    改行をカウントしないように修正

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Windows.Forms;
 using System.Drawing;
 using System.Globalization;
+using System.Linq;
 
 namespace ClipboardCharCountNotify
 {
@@ -23,7 +24,10 @@ namespace ClipboardCharCountNotify
                     string text = Clipboard.GetText();
                     if (!string.IsNullOrEmpty(text))
                     {
-                        int charCount = new StringInfo(text).LengthInTextElements;
+                        // 改行をカウントせず、空白はカウントする
+                        int charCount = text
+                            .Where(c => c != '\r' && c != '\n') // 改行文字（CR, LF）を除外
+                            .Count(c => !char.IsControl(c) && c != '\u200B'); // 制御文字とゼロ幅スペースを除外
                         notify.ShowBalloonTip(3000, "", $"クリップボード文字数: {charCount}", ToolTipIcon.Info);
                     }
                     else
@@ -42,7 +46,7 @@ namespace ClipboardCharCountNotify
             }
             finally
             {
-                System.Threading.Thread.Sleep(3000); // 通知表示時間
+                System.Threading.Thread.Sleep(3000);
                 notify.Visible = false;
                 notify.Dispose();
             }
